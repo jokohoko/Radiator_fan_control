3e02a979d44f0b50be734f11b329eb8cc45bf326
jokohoko <jokohoko13@hotmail.com>
2022-12-14 12:15:44 +0100

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,33 @@
+#include <OneWire.h>
+#include <DS18B20_INT.h>
+
+#define ONE_WIRE_BUS    8
+#define output          9
+
+OneWire     oneWire(ONE_WIRE_BUS);
+DS18B20_INT sensor(&oneWire);
+
+void setup() {
+  // put your setup code here, to run once
+  sensor.begin();
+  Serial.begin(9600);
+  pinMode(output, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.println("start");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor.requestTemperatures();
+
+  while (!sensor.isConversionComplete());  // (BLOCKING!!) wait until sensor is ready
+  Serial.println(sensor.getTempC());
+  if (sensor.getTempC() > 25) {
+    analogWrite(output, 200);
+    digitalWrite(13, HIGH);
+  } else {
+    analogWrite(output, 0);
+    digitalWrite(13, LOW);
+  }
+  delay(5000);
+}
